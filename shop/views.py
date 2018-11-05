a3b2cbd272682eca7435888c4b4134c8e7c01d76
egor <golovach.lena.olegovna@gmail.com>
2018-11-05 12:54:25 +0300

    moved some logic from templates to views

--- shop/views.py
+++ shop/views.py
@@ -4,9 +4,17 @@ from django.shortcuts import  render
 
 def shop_items_list(request):
     template = 'shop/itemlist.html'
+    button_list = []
+    background_list = []
     player = PlayerInventory.objects.get(name=request.user.id)
-    buttons = ClickButton.objects.all().order_by('btn_price')
-    backgrounds = Background.objects.all().order_by('bkg_price')
+    all_buttons = ClickButton.objects.all().order_by('btn_price')
+    for item in all_buttons:
+        if player.total_clicks >= item.btn_price:
+            button_list.append(item)
+    all_backgrounds = Background.objects.all().order_by('bkg_price')
+    for item in all_backgrounds:
+        if player.total_clicks >= item.bkg_price:
+            background_list.append(item)
     if request.POST.get('apply-button'):
         player.equipped_button = ClickButton.objects.get(id=request.POST.get('apply-button'))
         player.save()
@@ -17,7 +25,7 @@ def shop_items_list(request):
         player = PlayerInventory.objects.get(name=request.user.id)
     context = {
         'player': player,
-        'button_list': buttons,
-        'background_list': backgrounds
+        'button_list': button_list,
+        'background_list': background_list
     }
     return render(request, template, context)
